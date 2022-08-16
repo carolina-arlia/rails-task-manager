0432fa9c30d9bd1bcb8cdbbff6eff52d480843c4
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-16 19:02:54 +0200

    route delete tasks/:id

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update"
 
+  #delete
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
