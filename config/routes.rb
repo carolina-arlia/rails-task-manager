a495444f29ef5f0074e679b39f732f6e75685be6
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-16 18:29:56 +0200

    route tasks/new

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  #create
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  # read
   get "tasks", to: "tasks#index", as: :tasks
   get "tasks/:id", to: "tasks#show", as: :task
 
