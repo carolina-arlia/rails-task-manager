0b1fc3666c3c92d96ae8ad4d848d77f5e9af6807
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-16 17:57:34 +0200

    generate controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_153405) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_153911) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
